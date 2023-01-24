95cd622841dbc4ed6fe92df89074e7a6256c773f
Erfan Moeini <ec.moeini01@gmail.com>
2023-01-24 11:54:19 +0100

    Finalize assignment 2

--- mapper1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""mapper1.py"""
-
-import sys
-
-for line in sys.stdin:
-    line = line.strip()
-    activity, timestamp, lifecycle_trans, exec_id = line.split("\t")
-    print("%s\t%s\t%s" % (exec_id, activity, timestamp))
